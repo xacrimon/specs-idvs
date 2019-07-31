0a61720de65311840804cce31b4344747dff1df0
Acrimon <joel.wejdenstal@gmail.com>
2019-07-31 11:22:30 +0200

    Fix crazy mem growth.

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,9 @@ impl<T> IDVStorage<T> {
     unsafe fn check_prefill(&mut self, idx_cap: usize) {
         if idx_cap / SPARSE_RATIO > self.inner.len() {
             let additional = (idx_cap / SPARSE_RATIO).saturating_sub(self.inner.len());
-            self.inner.reserve(additional);
+            if self.inner.capacity() < self.inner.len() + 8 {
+                self.inner.reserve(additional);
+            }
             while self.inner.len() / SPARSE_RATIO < idx_cap {
                 self.inner.push(InterleavedGroup::blank());
                 self.free_slots.push((self.inner.len() - 1) as u16);
